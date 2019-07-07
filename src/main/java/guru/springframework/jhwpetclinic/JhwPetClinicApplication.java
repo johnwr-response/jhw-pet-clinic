6a5cae78753b8966da01363e41f5270a03a0df5b
John Håvard Wraalsen <johnwr@responseweb.no>
2019-07-07 19:49:21 +0200

    Initial Project Add

---
+++ src/main/java/guru/springframework/jhwpetclinic/JhwPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jhwpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhwPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhwPetClinicApplication.class, args);
+    }
+
+}
