ed132b003002649f1af86e8fdf5a1bef121548d9
John Håvard Wraalsen <johnwr@responseweb.no>
2019-07-09 00:34:00 +0200

    Added PetTypes to bootstrap. Closes #34

--- pet-clinic-web/src/main/java/guru/springframework/jhwpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/jhwpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.jhwpetclinic.bootstrap;
 
 import guru.springframework.jhwpetclinic.model.Owner;
+import guru.springframework.jhwpetclinic.model.PetType;
 import guru.springframework.jhwpetclinic.model.Vet;
 import guru.springframework.jhwpetclinic.services.OwnerService;
+import guru.springframework.jhwpetclinic.services.PetTypeService;
 import guru.springframework.jhwpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,32 +14,41 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        createOwner("Michael", "Weston");
-        createOwner("Fiona", "Glenanne");
+        PetType dog = petTypeService.save(createPetType("Dog"));
+        PetType cat = petTypeService.save(createPetType("Cat"));
+        ownerService.save(createOwner("Michael", "Weston"));
+        ownerService.save(createOwner("Fiona", "Glenanne"));
         System.out.println("Loaded Owners...");
-        createVet("Sam", "Axe");
-        createVet("Jessie", "Porter");
+        vetService.save(createVet("Sam", "Axe"));
+        vetService.save(createVet("Jessie", "Porter"));
         System.out.println("Loaded Vets...");
     }
-    private void createOwner(String firstName, String lastName) {
+    private PetType createPetType(String name) {
+        PetType pettype = new PetType();
+        pettype.setName(name);
+        return pettype;
+    }
+    private Owner createOwner(String firstName, String lastName) {
         Owner owner = new Owner();
         owner.setFirstName(firstName);
         owner.setLastName(lastName);
-        ownerService.save(owner);
+        return owner;
     }
-    private void createVet(String firstName, String lastName) {
+    private Vet createVet(String firstName, String lastName) {
         Vet vet = new Vet();
         vet.setFirstName(firstName);
         vet.setLastName(lastName);
-        vetService.save(vet);
+        return vet;
     }
 
 }
