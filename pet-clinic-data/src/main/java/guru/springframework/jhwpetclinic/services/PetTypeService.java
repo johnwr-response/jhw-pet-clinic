e059b89904ad7f0a9c3de9c1ece1f862f2d362a6
John Håvard Wraalsen <johnwr@responseweb.no>
2019-07-09 00:22:03 +0200

    Added PetTypeService. Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/jhwpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.jhwpetclinic.services;
+
+import guru.springframework.jhwpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
