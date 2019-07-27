c4fe0a4ac63ee343a4c26dcd1e2083ecd742b274
John Håvard Wraalsen <johnwr@responseweb.no>
2019-07-27 17:12:19 +0200

    Impl find owner. Closes #56

--- pet-clinic-data/src/main/java/guru/springframework/jhwpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/jhwpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,9 @@ package guru.springframework.jhwpetclinic.repositories;
 import guru.springframework.jhwpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/guru/springframework/jhwpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/jhwpetclinic/services/OwnerService.java
@@ -2,6 +2,9 @@ package guru.springframework.jhwpetclinic.services;
 
 import guru.springframework.jhwpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
